7f44c8567694dcf00cc715ae88aa5c779dec7476
liwaynel <wayne_li_3@sfu.ca>
2021-06-20 21:16:17 -0700

    everything but delete working

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -54,6 +54,7 @@ public class Main {
     String index(Map<String, Object> model) {
         try (Connection connection = dataSource.getConnection()) {
             Statement stmt = connection.createStatement();
+            stmt.executeUpdate("CREATE TABLE IF NOT EXISTS rectangles (id serial, name varchar(20), color varchar(20), area int, len int, wid int)");
             ResultSet rs = stmt.executeQuery("SELECT * FROM rectangles");
 
             ArrayList<Record> output = new ArrayList<Record>();
